e72ec5a22353da0b881d0a4ec58ec27f1b794e53
bo chen <chenb1@ufl.edu>
2020-01-22 20:06:41 -0500

    first three io for lab 2

--- Lab 2/main.cpp
+++ Lab 2/main.cpp
@@ -3,3 +3,24 @@
 #include <string>
 #include <vector>
 using namespace std;
+
+int main()
+{
+	string fileTitle;
+	string col1Header;
+	string col2Header;
+
+	cout << "Enter a title for the data: " << endl;
+	getline(cin, fileTitle);
+	cout << "You entered: " << fileTitle << endl;
+
+	cout << "Enter the column 1 header: " << endl;
+	getline(cin, col1Header);
+	cout << "You entered: " << col1Header << endl;
+
+	cout << "Enter the column 2 header: " << endl;
+	getline(cin, col2Header);
+	cout << "You entered: " << col2Header << endl;
+
+	return 0
+}
